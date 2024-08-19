06d23e4b0e281ad381fbe9dacef0db60030901c6
ThamanGurung01 <tgurung519@gmail.com>
2024-08-19 12:31:53 +0545

    simple ui updated with check & delete func.

--- src/Components/ToDoList.jsx
+++ src/Components/ToDoList.jsx
@@ -1,14 +1,13 @@
 import React,{useState,useContext,useEffect} from "react";
 import { InputTaskContext } from "../App";
 import { isClickedContext } from "../App";
-
-
 function ToDoList(){
-  // const DoLists=["Eat breakfast","Excercise","Shower"];
+  // const DoLists=[{task:"Eat breakfast",isChecked:false},{task:"Excercise",isChecked:false},{task:"Shower",isChecked:false}];
+
   const [Lists,setLists]=useState([]);
   const [newInputTasks,setInputLists]=useContext(InputTaskContext);
   const [isAddBtnClicked,setIsClicked]=useContext(isClickedContext);
-
+  
   useEffect(()=>{
     if(isAddBtnClicked){
       if(newInputTasks!=""){
@@ -18,15 +17,24 @@ function ToDoList(){
   },[isAddBtnClicked]);
 
   const AddTaskList=()=>{
-    setLists([...Lists,newInputTasks]);
+    setLists([...Lists,{task:newInputTasks,isChecked:false}]);
     setInputLists("");
     setIsClicked(false);
   }
 
+  const FinishedTaskHandler=(index)=>{
+    const updatedList=Lists.map((el,i)=> index!=i?el: {task:el.task,isChecked:!el.isChecked})
+      setLists(updatedList);
+    // console.log(updatedList);
+  }
+  const deleteTask=(index)=>{
+    const updatedList=Lists.filter((_,i)=>index!=i);
+    setLists(updatedList);
+  }
   return(
     <div className="ToDoList">
       <ul className="Lists">
-      {Lists.map((element,index)=> <li key={index}>{element}</li> )}
+      {Lists.map((element,index)=> <li key={index}>{element.task} <input type="checkbox" value={element.task} onClick={()=>FinishedTaskHandler(index)} /> <img src="./src/assets/delete.png"  onClick={()=>deleteTask(index)}/></li> )}
       </ul>
     </div>
   );
