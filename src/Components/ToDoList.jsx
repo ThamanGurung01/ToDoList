ac8ac07566fdc1c9ed2e3c1a9afd5a047b550262
ThamanGurung01 <tgurung519@gmail.com>
2024-08-19 22:24:33 +0545

    small update on css

--- src/Components/ToDoList.jsx
+++ src/Components/ToDoList.jsx
@@ -1,6 +1,7 @@
 import React,{useState,useContext,useEffect} from "react";
 import { InputTaskContext } from "../App";
 import { isClickedContext } from "../App";
+import DeleteImage from '../assets/delete.png'
 function ToDoList(){
   // const DoLists=[{task:"Eat breakfast",isChecked:false},{task:"Excercise",isChecked:false},{task:"Shower",isChecked:false}];
 
@@ -35,7 +36,7 @@ function ToDoList(){
     <div className="ToDoList">
       <h1>ToDoLists</h1>
       <ul className="Lists" style={{paddingBottom:`${Lists.length>0?"1rem":""}`}}>
-      {Lists.map((element,index)=> <li style={{textDecoration: `${element.isChecked?"line-through":""}`}} key={index}><div><input type="checkbox" value={element.task} onClick={()=>FinishedTaskHandler(index)} /><span>{element.task}</span></div><img src="./src/assets/delete.png"  onClick={()=>deleteTask(index)}/></li> )}
+      {Lists.map((element,index)=> <li style={{textDecoration: `${element.isChecked?"line-through":""}`}} key={index}><div><input type="checkbox" value={element.task} onClick={()=>FinishedTaskHandler(index)} /><span>{element.task}</span></div><img src={DeleteImage} draggable={false} onClick={()=>deleteTask(index)}/></li> )}
       </ul>
     </div>
   );
